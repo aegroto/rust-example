2f3bfe415a4fbdc84acd0a8aa38a24e944211283
aegroto <aegroto@protonmail.com>
2021-06-01 22:07:17 +0200

    entries loading

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::env;
 use std::io::BufRead;
 use std::io::BufReader;
 
+struct Entry {
+    angle: u8,
+    new_angle: u8,
+    hidden_value: u8
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -15,7 +21,25 @@ fn main() {
 
     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
-        println!("{}", line.unwrap());
+    let mut entries: Vec<Entry> = Vec::new();
+
+    let lines = reader.lines().skip(1);
+
+    let value_parser = |fragment: &str| 
+        fragment.parse::<u8>().unwrap();
+
+    for line in lines {
+        let line = line.unwrap();
+        let fragments: Vec<&str> = line.split(',').collect();
+
+        let entry = Entry {
+            angle: value_parser(fragments[0]),
+            new_angle: value_parser(fragments[1]),
+            hidden_value: value_parser(fragments[2])
+        };
+
+        entries.push(entry);
     }
+
+
 }
